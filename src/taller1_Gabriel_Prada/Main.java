0826ed68d22f5a4c69752f55298776f36222fbcb
iceman <gaboltio108@gmail.com>
2025-09-07 00:08:34 -0500

    Fix formatting issues by adding newlines at the end of multiple class files

--- src/taller1_Gabriel_Prada/Main.java
+++ src/taller1_Gabriel_Prada/Main.java
@@ -1,10 +1,4 @@
 package taller1_Gabriel_Prada;
-
-import javax.swing.*;
-import java.util.Scanner;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //Crear persona
